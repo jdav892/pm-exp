93523e1cd49527a2ced30d925ba035dc27ba2187
jdav892 <jadavila9@gmail.com>
2024-11-09 04:05:10 -0500

    after implementing darkmode buttons now working towards making sidebar responsive

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -21,11 +21,15 @@ const DashboardLayout = ({ children } : {children: React.ReactNode}) => {
       document.documentElement.classList.remove("dark")
     }
   })
-
+//checking size of navbar to collapse it
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
     <Sidebar />
-    <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+    
+    <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+      isSidebarCollapsed ? "" : "md:pl-64"
+      }`}
+    >
         <Navbar />
         {children}
     </main>
