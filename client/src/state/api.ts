160fad126053b5da1be2e4aa86146614f5c9d335
jdav892 <jadavila9@gmail.com>
2024-11-14 02:00:36 -0500

    declares types for projects and tasks in api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
+export interface Project {
+    id: number;
+    name: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+}
+
+export interface Task {
+    title: string;
+    description?: string;
+    status?: string;
+    priority?: string;
+    tags?: string;
+    startDate?: string;
+    dueDate?: string;
+    points: number;
+    projectId: number;
+    authorUser: number;
+    assignUserId: number;
+}
+
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
     tagTypes: [],
-    endpoints: (build) => ({}),
+    endpoints: (build) => ({
+        
+    }),
 })
 
 export const {} = api
