a35c16617f1051388823f8d064c61593cecfa07b
jdav892 <jadavila9@gmail.com>
2024-11-14 02:53:49 -0500

    moves component file to src directory and starts to create projects page

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    params: { id: string };
+}
+
+const Project = ({ params }: Props) => {
+
+    
+    return <div>Project</div>
+}
+
+export default Project 
