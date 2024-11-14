f737799b49566b0a6c039097c65c91c801576bd4
jdav892 <jadavila9@gmail.com>
2024-11-14 02:40:31 -0500

    creates functions for creating project and task lists that update the list when called

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -56,8 +56,8 @@ export interface Task {
 
     author?: UserActivation;
     assignee?: UserActivation;
-    comments?: Comment();
-    attachments?: Attachment();
+    comments?: Comment[];
+    attachments?: Attachment[];
 
 }
 
@@ -65,10 +65,36 @@ export interface Task {
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes: [],
+    tagTypes: ["Projects", "Tasks"],
     endpoints: (build) => ({
-
+        getProjects: build.query<Project[], void>({
+            query: () => "projects",
+            providesTags: ["Projects"],
+        }),
+        createProject: build.mutation<Project, Partial<Project>>({
+            query: (project) => ({
+                url: "projects",
+                method: "POST",
+                body: project
+            }),
+            //to fetch updated list of projects on call
+            invalidatesTags: ["Projects"]
+        }),
+        getTasks: build.query<Task[], { projectId: number }>({
+            query: ({ projectId }) => `tasks?projectId=${projectId}`,
+            providesTags: (result) => result ? result.map(( {id}) => ({ type: "Tasks" as const, id}))
+            : [{ type:"Tasks" as const }],
+        }),
+        createTasks: build.mutation<Task, Partial<Task>>({
+            query: (task) => ({
+                url: "tasks",
+                method: "POST",
+                body: task,
+            }),
+            //same as above to update list of tasks when creating a new list of tasks
+            invalidatesTags: ["Tasks"]
+        })
     }),
 })
 
-export const {} = api
+export const { useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTasksMutation } = api
