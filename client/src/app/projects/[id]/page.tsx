b2bb155d2e483ab7f68024232bd0ca85787fa3ad
jdav892 <jadavila9@gmail.com>
2024-11-17 16:11:57 -0500

    renders header on projects as well as filter and share buttons added to board with table list and timeline

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,21 +1,22 @@
 "use client";
-
-import React, {useState} from 'react'
-import ProjectHeader from '@/app/projects/ProjectHeader'
+import React, { useState } from 'react'
+import ProjectHeader from "@/app/projects/ProjectHeader"
+import Board from "../BoardView"
 
 type Props = {
     params: { id: string };
-};
+}
 
 const Project = ({ params }: Props) => {
     const { id } = params;
     const [activeTab, setActiveTab] = useState("Board");
     const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
-    console.log("Rendering project with ID:", id);
-  return (
+    return (
     <div>
-        {/*MODAL NEW TASK*/}
         <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        { activeTab === "Board" && (
+            <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+        )}
     </div>
   );
 };
