52a09f29a3d1c1f008befbf10801f764e064f4fa
jdav892 <jadavila9@gmail.com>
2024-11-18 03:32:46 -0500

    have created a list view for tasks and assignments with authors and assignees

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -1,3 +1,6 @@
+import Header from '@/components/Header';
+import TaskCard from "@/components/TaskCard";
+import { Task, useGetTasksQuery } from '@/state/api';
 import React from 'react'
 
 type Props = {
@@ -5,9 +8,25 @@ type Props = {
     setIsModalNewTaskOpen: (isOpen: boolean) => void;
 }
 
-const ListView = (props: Props) => {
-  return (
-    <div>ListView</div>
+const ListView = ({ id, setIsModalNewTaskOpen}: Props) => {
+    const {
+        data: tasks,
+        error,
+        isLoading,
+    } = useGetTasksQuery({ projectId: Number(id) });
+    
+    if(isLoading) return <div>Loading....</div>
+    if(error) return <div>An error occurred while fetching tasks</div>
+    
+    return (
+    <div className="px-4 pb-8 xl:px-6">
+        <div className="pt-5">
+            <Header name="List" />
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
+            {tasks?.map((task:Task) => <TaskCard key={task.id} task={task} />)}
+        </div>
+    </div>
   )
 }
 
