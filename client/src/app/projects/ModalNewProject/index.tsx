8d91cf958a81a58852df23dacd4298170a9eef37
jdav892 <jadavila9@gmail.com>
2024-11-19 04:22:25 -0500

    created new projects input field, now have to implement http request

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,5 +1,6 @@
 import { useCreateProjectMutation } from '@/state/api';
 import React, { useState } from 'react'
+import Modal from '@/components/Modal';
 
 type Props = {
     isOpen: boolean;
@@ -12,7 +13,6 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
     const [description, setDescription] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
-    const [teamId, setTeamId] = useState("");
 
     const handleSubmit = async () => {
         if(!projectName || !startDate || !endDate) return;
@@ -29,9 +29,56 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
         return projectName && description && startDate && endDate;
     }
 
+    const inputStyles = "w-full rounded border border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
+
+
   return (
-    <div>ModalNewProject</div>
-  )
+    <Modal isOpen={isOpen} onClose={onClose} name="Create New Project">
+        <form
+            className='mt-4 space-y-6'
+            onSubmit={(userInput) => {
+                userInput.preventDefault();
+                handleSubmit();
+            }}
+            >
+                <input type="text"
+                 className={inputStyles} 
+                 placeholder="Project Name" 
+                 value={projectName} 
+                 onChange={(userInput) => setProjectName(userInput.target.value)}
+                />
+                <textarea
+                 className={inputStyles} 
+                 placeholder="Description" 
+                 value={description} 
+                 onChange={(userInput) => setDescription(userInput.target.value)}
+                />
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2">
+                <input type="date"
+                 className={inputStyles} 
+                 placeholder="Start Date" 
+                 value={startDate} 
+                 onChange={(userInput) => setStartDate(userInput.target.value)}
+                />
+                <input type="date"
+                 className={inputStyles} 
+                 placeholder="Due Date" 
+                 value={endDate} 
+                 onChange={(userInput) => setEndDate(userInput.target.value)}
+                />
+            </div>
+            <button
+                type="submit"
+                className={`mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus-offset-2 ${
+                    !isFormValid() || isLoading ? "cursor-not-allowed opacity-50" : ""
+                    }`}
+                    disabled={!isFormValid() || isLoading}
+                    >
+                {isLoading ? "Creating..." : "Create Project"}
+            </button>
+        </form>
+    </Modal>
+)
 }
 
 export default ModalNewProject 
