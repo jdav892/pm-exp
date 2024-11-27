e6d264b20eaf4f376fd277543e36db8fc19fe5c4
jdav892 <jadavila9@gmail.com>
2024-11-27 06:44:38 -0500

    feat: home page component now renders for home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,7 @@
-//import Image from "next/image";
+import HomePage from "@/components/home/page";
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        we're live
-      </main>
+    <HomePage />
   );
 }
