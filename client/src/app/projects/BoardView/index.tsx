8aac01ebfccede51105dc3e1dbec0c730f094595
jdav892 <jadavila9@gmail.com>
2025-03-25 04:09:42 -0400

    feat: updates next version to patch middleware issue

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { DndProvider, useDrag, useDrop } from "react-dnd"
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Task as TaskType } from "@/state/api"
-import { EllipsisVertical, Italic, MessageSquare, MessageSquareMore, Plus } from 'lucide-react';
+import { EllipsisVertical, MessageSquareMore, Plus } from 'lucide-react';
 import { format } from 'date-fns'
 import Image from 'next/image';
 
